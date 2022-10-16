d3f3ea7ab0e316bdcf4c1353bb92c8318b22fb04
Yusuf Manshur <ymanshur@gmail.com>
2022-10-17 06:46:58 +0700

    feat(ProductHandler): modified products data

--- handler/handler.go
+++ handler/handler.go
@@ -74,9 +74,9 @@ func ProductHandler(w http.ResponseWriter, r *http.Request) {
 	data := map[string]any{
 		"title": "I'm Learning Golang Web | Product",
 		"products": []entity.Product{
-			{ID: 1, Name: "Mobilio", Price: 220000000, Stock: 3},
-			{ID: 2, Name: "Xpander", Price: 270000000, Stock: 2},
-			{ID: 3, Name: "Pajero Sport", Price: 500000000, Stock: 4},
+			{ID: 1, Name: "Mobilio", Price: 220000000, Stock: 11},
+			{ID: 2, Name: "Xpander", Price: 270000000, Stock: 8},
+			{ID: 3, Name: "Pajero Sport", Price: 500000000, Stock: 1},
 		},
 	}
 
