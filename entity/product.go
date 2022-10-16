6b5201f5b986aa940274ac5ec3ac6d15cb7bba5f
Yusuf Manshur <ymanshur@gmail.com>
2022-10-17 06:46:28 +0700

    feat(product): addded StockStatus method

--- entity/product.go
+++ entity/product.go
@@ -6,3 +6,15 @@ type Product struct {
 	Price int
 	Stock int
 }
+
+func (product Product) StockStatus() string {
+	var status string
+
+	if product.Stock < 3 {
+		status = "Stock hampir habis!"
+	} else if product.Stock < 10 {
+		status = "Stock terbatas"
+	}
+
+	return status
+}
